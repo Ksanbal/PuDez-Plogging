a57fce8a8a9837a15ffe2f3fd5c3987f44434c75
Ksanbal <dev.ksanbal@gmail.com>
2024-10-31 03:25:04 +0900

    ✨ Firestore 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:pudez_plogging/firebase_options.dart';
 import 'package:pudez_plogging/src/common/font.family.dart';
 import 'package:pudez_plogging/src/controllers/home/home.controller.dart';
 import 'package:pudez_plogging/src/controllers/onboarding/onboarding.controller.dart';
@@ -8,7 +10,12 @@ import 'package:pudez_plogging/src/views/home/home.view.dart';
 import 'package:pudez_plogging/src/views/onboarding/onboarding.view.dart';
 import 'package:pudez_plogging/src/views/splash.view.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
